dbf26a85cec64615a3a0fce07be108ef2154e3ec
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-02-14 23:49:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    const prices = document.querySelectorAll('.price');
+    let total = 0;
+
+    prices.forEach(price => {
+        total += parseFloat(price.textContent);
+    });
+
+    const table = document.querySelector('table');
+    const newRow = document.createElement('tr');
+    const newCell = document.createElement('td');
+
+    newCell.setAttribute('colspan', 2);
+    newCell.textContent = `Total Price: Rs ${total}`;
+
+    newRow.appendChild(newCell);
+    table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
